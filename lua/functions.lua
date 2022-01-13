47ac3935bf36b04c27aa4ab4d229f01763c1fc74
jinzaizhichi <jinzaizhichi9888@gmail.com>
2022-01-13 21:01:20 +0800

    functions.lua修改

--- lua/functions.lua
+++ lua/functions.lua
@@ -1,4 +1,5 @@
-vim.cmd [[
+vim.cmd
+[[
 function! RipgrepFzf(query, fullscreen)
   let command_fmt = 'rg --column --line-number --no-heading --color=always --smart-case -- %s || true'
   let initial_command = printf(command_fmt, shellescape(a:query))
